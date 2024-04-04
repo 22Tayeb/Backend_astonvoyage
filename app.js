390a9798787cf159441e9df6f8699467a7b86bb7
22Tayeb <tayeb.kharbouch27@gmail.com>
2024-04-04 14:56:27 +0200

    fix de updateDest

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser  from "body-parser";
 import { RouterUser } from "./router/user.router.js";
-import { RouterPost } from './router/post.router.js';
 import { RouterDest } from './router/destination.router.js';
+import multer from 'multer';
 import cors  from "cors";
 import { config as dotenvConfig } from 'dotenv';
+import path,{dirname} from 'path';
+import { fileURLToPath } from 'url';
+
 dotenvConfig({path: './config/.env'})
+
 
 const app = express();
 // Cors options 
@@ -15,6 +19,18 @@ const corsOptions = {
     credentials: true,
     optionSuccessStatus: 200
 }
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+
+const upload = multer({ storage: storage })
+//config multer
+
 app.use(cors(corsOptions));
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
@@ -26,6 +42,7 @@ app.use(function (req, res, next) {
 });
 
 
+
 //conection to my BDD
 mongoose.connect('mongodb+srv://'+process.env.DB_USER+':'+process.env.DB_PASS+'@astonvoyage.re5jhkv.mongodb.net/?retryWrites=true&w=majority&appName=AstonVoyage',{
     useNewUrlParser: true, useUnifiedTopology: true
@@ -33,31 +50,39 @@ mongoose.connect('mongodb+srv://'+process.env.DB_USER+':'+process.env.DB_PASS+'@
     console.log('Connexion réussi à MangoDB');
 }).catch((err) => console.log("failed to connect to MangoDB" ,err));
 
-
-
 app.use(bodyParser.json());
 // API USER
 app.use('/api/user/', RouterUser.createUsr);
 //app.use('/api/user/', RouterUser.authenticateUser);
 
 // API DESTINATION
+app.get('/api/destination/download/:filename', (req,res) => {
+    const fileName = req.params.filename;
+        const __dirname = dirname(fileURLToPath(import.meta.url));
+
+    const filePath = path.join(__dirname, 'uploads', fileName);
+
+
+    //envoi du fichier en reponse
+    res.download(filePath, fileName, (err) => {
+        if(err) {
+            //handle error
+            console.error('Error downloading file:', err);
+            res.status(500).send('error downloading file');
+        }
+    });
+});
+app.post('/api/destination/upload', upload.single('file'), (req, res) => {
+  // ... gérer le fichier reçu ici ...
+  res.status(200).send('')
+});
+
+
 app.use('/api/destination/', RouterDest.createDestination);
-// API POST
-app.use('/api/post/', RouterPost.createPost);
-app.use('/api/post/', RouterPost.getPosts);
-app.use('/api/post/', RouterPost.getPostById);
-app.use('/api/post/', RouterPost.deletePost);
-app.use('/api/post/', RouterPost.updatePostById);
-app.use('/api/post/', RouterPost.likedPost)
 
 app.get("/", (req,res) => {
     res.send('Bienvenue sur le backend de AstonVoyage');
 });
-
-//ouvrir la route /api/home pour afficher ce texte
-// app.get("/api/home", (req,res) => {
-//     res.send("Bienvenue sur la page principal d'AstonVoyage");
-// });
 
 // Acces serveur 
 app.listen(process.env.PORT, () => {
