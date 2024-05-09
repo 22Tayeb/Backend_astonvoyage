939f0590844c929d7467f099c5a4ac457029d4f5
Jaeltaka <takasjaels@yahoo.com>
2024-05-09 13:58:02 +0200

    ajout de l'api mes reservation

--- controllers/booking-destination.controller.js
+++ controllers/booking-destination.controller.js
@@ -1,28 +1,31 @@
 import bookingdestination from "../models/booking-destination.model.js";
+import mongoose from 'mongoose';
 
-// function create user
-export const createBookingDestCtrl = async (req,res)=>{
 
-    const book = new bookingdestination(req.body)
-    console.log(req.body)
-    try{
-        const response = await book.save()
-        res.status(201).json({response,message: 'Booking crée'})
-    }catch(err){
-        res.status(500).json({erreur:"Booking impossible"})
-    }
+
+export const getAllBook = async (req, res) => {
+    const allbooking = await bookingdestination.find({});
+    res.status(200).json(allbooking)
 }
 
-
-    export const getAllBook = async (req,res) => {
-        const allbooking = await bookingdestination.find({});
-        res.status(200).json(allbooking)
+export const getAllBookByUser = async (req, res) => {
+    const userId = req.params.userId;
+    try {
+        const books = await bookingdestination.find({ userId }).populate('destinationId').exec();
+        res.status(200).json(books)
+    } catch (e) {
+        res.status(500).json(e)
     }
-    
-    export const getAllBookByUser = async (req,res) => {
-        const userId = req.params.userId
-        console.log('ihih',userId)
-
-        const allbooking = await bookingdestination.find({user:userId});
-        res.status(200).json(allbooking)
+}
+export const createMyBooking = async (req, res) => {
+    //console.log(req.body)
+    const userId = new mongoose.Types.ObjectId(req.body.userId)
+    const destinationId = new mongoose.Types.ObjectId(req.body.destinationId)
+    const myBooking = new bookingdestination({ userId, destinationId }) //{userId:4355435, destinationId:3555355}
+    try {
+        const response = await myBooking.save()
+        res.status(201).json({ response, message: 'Booking réservé, paiement accepte' })
+    } catch (err) {
+        res.status(500).json({ erreur: "Booking non réservé, paiement refuse" })
     }
+}
--- models/booking-destination.model.js
+++ models/booking-destination.model.js
@@ -1,23 +1,9 @@
-import  mongoose from 'mongoose';
+import mongoose from 'mongoose';
 
 // Model of my document for my MangoDB BDD
 const bookingdestination = mongoose.Schema({
-    nom_destination:{
-        type: String,
-        required: true 
-    },
-    description:{
-        type: String,
-        required: true
-    },
-    prix:{
-        type:String,
-        required:true
-    },
-    user:{
-        type : mongoose.Schema.Types.ObjectId,
-        ref:'User',
-    }
+    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    destinationId: {type: mongoose.Schema.Types.ObjectId, ref: 'Destination'},
 
 });
 
